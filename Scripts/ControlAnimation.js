1a4a8bc209c997d402895e750334ff7c79c633c3
Willy Bardales <willybc06@gmail.com>
2017-03-25 12:04:07 -0300

    Sonido en el juego

--- Scripts/ControlAnimation.js
+++ Scripts/ControlAnimation.js
@@ -1,7 +1,16 @@
 ﻿#pragma strict
 
+var m_sonido1 : AudioClip;
+var m_camara : AudioListener;
+
+
+var m_miSonido : AudioClip;
+var m_miOrigen : AudioSource;
 
 var m_animador : Animator;
+
+var m_inicioToque : Vector2;
+
 function Start () {
     m_animador = gameObject.GetComponent(Animator);
 }
@@ -9,6 +18,7 @@ function Start () {
 function Update () {
     if (Input.GetKeyDown(KeyCode.Alpha1)){
         m_animador.SetInteger("Direccion",1);
+        m_miOrigen.PlayOneShot(m_miSonido);
     }
     if (Input.GetKeyDown(KeyCode.Alpha2)){
         m_animador.SetInteger("Direccion",2);
@@ -19,5 +29,5 @@ function Update () {
     if (Input.GetKeyDown(KeyCode.Alpha4)){
         m_animador.SetInteger("Direccion",4);
     }
-    
+
 }
