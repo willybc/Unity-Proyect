4c635ce88bc857076720de408a864a93cdd1ec46
Willy Bardales <willybc06@gmail.com>
2017-03-24 22:27:30 -0300

    Sistema de animaciones

---
+++ Scripts/ControlAnimation.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+
+var m_animador : Animator;
+function Start () {
+    m_animador = gameObject.GetComponent(Animator);
+}
+
+function Update () {
+    if (Input.GetKeyDown(KeyCode.Alpha1)){
+        m_animador.SetInteger("Direccion",1);
+    }
+    if (Input.GetKeyDown(KeyCode.Alpha2)){
+        m_animador.SetInteger("Direccion",2);
+    }
+    if (Input.GetKeyDown(KeyCode.Alpha3)){
+        m_animador.SetInteger("Direccion",3);
+    }
+    if (Input.GetKeyDown(KeyCode.Alpha4)){
+        m_animador.SetInteger("Direccion",4);
+    }
+    
+}
